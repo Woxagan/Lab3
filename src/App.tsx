7c89dcacfe6232284d434592cd39dd4f79a7404d
Woxa <neagudumitru26@gmail.com>
2024-04-24 22:10:22 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './Cockie.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         Neagu Dumitru
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
